1f0b65ed6bff6854dc0c1bcf854ab2b61c101e51
pkyh2 <pkyh1@naver.com>
2022-05-30 17:18:33 +0900

    add nodemon

--- app/app.js
+++ app/app.js
@@ -4,11 +4,12 @@ const express = require("express")
 const app = express();
 
 // routing 현재 routes home 폴더에 있는 js파일을 불러오라
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 // App
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs")
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // 미들웨어를 등록해주는 메서드
 
