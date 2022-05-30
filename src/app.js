dd4f900f1a010ff103ce14a82242357b3eafcc2a
pkyh2 <pkyh1@naver.com>
2022-05-30 15:51:22 +0900

    arrange folder tree

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@
 const express = require("express")
 const app = express();
 
-const PORT = 3000;
-
 // routing 현재 routes home 폴더에 있는 js파일을 불러오라
 const home = require("./routes/home")
 
@@ -14,8 +12,4 @@ app.set("view engine", "ejs")
 
 app.use("/", home); // 미들웨어를 등록해주는 메서드
 
-
-app.listen(PORT, () => {
-    console.log("서버가동!")
-    
-})
+module.exports = app
--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -2,16 +2,16 @@
 
 const home = (req, res) => {
     res.render("home/index")
-    console.log(typeof(req));
+    // console.log(typeof(req));
     // console.log(JSON.stringify(req.socket));
     // console.log(req._header);
-    console.log(req.socket.res);
-    console.log(res);
+    // console.log(req.socket.res);
+    // console.log(res);
 }
 
 const login = (req, res) => {
     res.render("home/login")
-    console.log("login debug")
+    // console.log("login debug")
 }
 
 // json 형태로 내보내는데 key값만 입력하면 value값은 key와 똑같은 값으로 입력된다.
